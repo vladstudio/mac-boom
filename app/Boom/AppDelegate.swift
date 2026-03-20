c01075fb2aef0edd2ff9d5bb223dbbbebf54dfc0
Vlad Gerasimov <vlad@vlad.studio>
2026-03-20 11:50:49 +0400

    Add login item toggle and menu delegate

--- app/Boom/AppDelegate.swift
+++ app/Boom/AppDelegate.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import ServiceManagement
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
@@ -9,11 +10,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if let button = statusItem.button {
             button.image = NSImage(systemSymbolName: "burst", accessibilityDescription: "Boom")
         }
+
         let menu = NSMenu()
+        menu.delegate = self
+        menu.addItem(NSMenuItem(title: "Start on Login", action: #selector(toggleLogin), keyEquivalent: ""))
+        menu.addItem(.separator())
         menu.addItem(NSMenuItem(title: "Quit Boom", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        menu.items.first?.target = self
         statusItem.menu = menu
 
         windowTracker = WindowTracker()
         windowTracker.start()
+    }
+
+    @objc private func toggleLogin(_ sender: NSMenuItem) {
+        let enabling = SMAppService.mainApp.status != .enabled
+        try? enabling ? SMAppService.mainApp.register() : SMAppService.mainApp.unregister()
+        sender.state = enabling ? .on : .off
+    }
+}
+
+extension AppDelegate: NSMenuDelegate {
+    func menuWillOpen(_ menu: NSMenu) {
+        menu.items.first?.state = SMAppService.mainApp.status == .enabled ? .on : .off
     }
 }
